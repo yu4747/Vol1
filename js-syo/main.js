db3caa63c0f6c28e1b0812408529472ff022a11d
yu4747 <yu200047@gmail.com>
2021-06-09 17:09:05 +0900

    second commit

--- js-syo/main.js
+++ js-syo/main.js
@@ -15,3 +15,36 @@ const items = document.querySelectorAll(".list li");
 items.forEach((item) => {
   console.log(item.textContent);
 });
+
+const elem = document.querySelector(".list");
+console.log(elem.textContent);
+console.log(elem.innerHTML);
+for (let i = 0; i < elem.children.length; i++) {
+  console.log(elem.children[i]);
+}
+console.log(elem.firstElementChild);
+console.log(elem.lastElementChild);
+console.log(elem.parentElement);
+
+const elem2 = document.querySelector("img");
+
+// console.log(elem2.getAttribute("src"));
+elem.setAttribute("src", "https://placehold.it/400×200");
+
+const newItem = document.createElement("li");
+newItem.textContent = "新しいアイテム";
+
+list.appendChild(newItem);
+list.removeChild(list.firstElementChild);
+
+const addButton = document.getElementById("add");
+const removeButton = document.getElementById("remove");
+// const list = document.querySelector(".list");
+addButton.addEventListener("click", (event) => {
+  const newItem = document.createElement("li");
+  newItem.textContent = "新しいアイテム";
+  list.appendChild(newItem);
+});
+removeButton.addEventListener("click", (event) => {
+  list.removeChild(list.lastElementChild);
+});
