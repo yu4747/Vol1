e0e5739af5d54155d064bc235fe0f79b937a58b1
yu4747 <yu200047@gmail.com>
2021-06-26 13:43:56 +0900

    fource commit

--- zyanken/main.js
+++ zyanken/main.js
@@ -9,34 +9,45 @@ decide.addEventListener("click", function () {
   const opponentNum = Math.floor(Math.random() * 3);
   if (opponentNum === 0) {
     opponent.textContent = "相手の手：グー";
+    console.log("oppo-rock");
   }
 
   if (opponentNum === 1) {
     opponent.textContent = "相手の手：チョキ";
+    console.log("oppo-scissors");
   }
 
   if (opponentNum === 2) {
     opponent.textContent = "相手の手：パー";
+    console.log("oppo-paper");
   }
 
   switch (myNum) {
     case "gu": {
       if (opponentNum === 0) {
         result.textContent = "結果：引き分け";
+        console.log("aiko");
       } else if (opponentNum === 1) {
         result.textContent = "結果：勝ち";
+        console.log("victory");
       } else {
         result.textContent = "結果：負け";
+        console.log("make");
       }
+      break;
     }
     case "tyoki": {
       if (opponentNum === 0) {
         result.textContent = "結果：負け";
+        console.log("make");
       } else if (opponentNum === 1) {
         result.textContent = "結果：引き分け";
+        console.log("aiko");
       } else {
         result.textContent = "結果：勝ち";
+        console.log("victory");
       }
+      break;
     }
     case "pa": {
       if (opponentNum === 0) {
