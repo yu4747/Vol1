dc5b97692cc5297b709620d48d78bb65fc4cc531
yu4747 <yu200047@gmail.com>
2021-08-20 21:21:07 +0900

    first commit

---
+++ day-counter/main.js
@@ -0,0 +1,18 @@
+const daycount = getElementById("count-p");
+daycount.textContent = `ドラえもんが生まれるまであと${doraemon}`;
+
+let doraemon = moment();
+
+// let fromDate = moment();
+// let toDate = moment("2112-09-03 12:00");
+
+// let doraemon = moment().add()
+
+// E: いくつかのアーカイブを取得できません。apt-get update を実行するか --fix-missing オプションを付けて試してみてください 。
+// leocho@DESKTOP-LE7C2NO:~/codevillage$ apt-get update
+// パッケージリストを読み込んでいます... 完了
+// E: ロックファイル /var/lib/apt/lists/lock をオープンできません - open (13: 許可がありません)
+// E: ディレクトリ /var/lib/apt/lists/ をロックできません
+// W: ファイル /var/cache/apt/pkgcache.bin の削除中に問題が発生しました - RemoveCaches (13: 許可がありません)
+// W: ファイル /var/cache/apt/srcpkgcache.bin の削除中に問題が発生しました - RemoveCaches (13: 許可がありません)
+//
