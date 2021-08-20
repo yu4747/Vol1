3afc31a57cbb4fd30778936ef276ad421a60931c
yu4747 <yu200047@gmail.com>
2021-08-20 22:27:40 +0900

    second commit

--- day-counter/main.js
+++ day-counter/main.js
@@ -1,18 +1,14 @@
-const daycount = getElementById("count-p");
-daycount.textContent = `ドラえもんが生まれるまであと${doraemon}`;
+const daycount = document.getElementById("count-p");
+let now = moment();
+let doraBirthday = moment("21120903", "YYYYMMDD");
+let doraBirthhour = moment("00:00");
+let day = doraBirthday.diff(moment(), "days");
+// let hour = doraBirthhour.diff(moment(), "seconds");
+// .format("YYYY年MM月DD日");
 
-let doraemon = moment();
+daycount.textContent = `ドラえもんが生まれるまであと${day}日`;
 
 // let fromDate = moment();
 // let toDate = moment("2112-09-03 12:00");
 
 // let doraemon = moment().add()
-
-// E: いくつかのアーカイブを取得できません。apt-get update を実行するか --fix-missing オプションを付けて試してみてください 。
-// leocho@DESKTOP-LE7C2NO:~/codevillage$ apt-get update
-// パッケージリストを読み込んでいます... 完了
-// E: ロックファイル /var/lib/apt/lists/lock をオープンできません - open (13: 許可がありません)
-// E: ディレクトリ /var/lib/apt/lists/ をロックできません
-// W: ファイル /var/cache/apt/pkgcache.bin の削除中に問題が発生しました - RemoveCaches (13: 許可がありません)
-// W: ファイル /var/cache/apt/srcpkgcache.bin の削除中に問題が発生しました - RemoveCaches (13: 許可がありません)
-//
