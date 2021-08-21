d56d209c96d93d8e97f89b34fa56f08ec50f7da8
yu4747 <yu200047@gmail.com>
2021-08-21 18:18:38 +0900

    third commit

--- day-counter/main.js
+++ day-counter/main.js
@@ -1,14 +1,17 @@
 const daycount = document.getElementById("count-p");
-let now = moment();
-let doraBirthday = moment("21120903", "YYYYMMDD");
-let doraBirthhour = moment("00:00");
-let day = doraBirthday.diff(moment(), "days");
-// let hour = doraBirthhour.diff(moment(), "seconds");
-// .format("YYYY年MM月DD日");
+function clock() {
+  let now = moment();
+  let doraBirthday = moment("21120903", "YYYYMMDD");
+  let doraBirthhour = moment("00:00");
+  let day = doraBirthday.diff(moment(), "days");
+  let hourB = doraBirthday.diff(moment(), "hours");
+  let hour = hourB % 24;
+  let minuteB = doraBirthday.diff(moment(), "minutes");
+  let minute = minuteB % 60;
+  let secondB = doraBirthday.diff(moment(), "seconds");
+  let second = secondB % 60;
 
-daycount.textContent = `ドラえもんが生まれるまであと${day}日`;
+  daycount.textContent = `ドラえもんが生まれるまであと${day}日${hour}時間${minute}分${second}秒`;
+}
 
-// let fromDate = moment();
-// let toDate = moment("2112-09-03 12:00");
-
-// let doraemon = moment().add()
+setInterval("clock()", 1000);
